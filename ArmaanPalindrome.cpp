5c2b681677ac0e9b335e933e8087022727e1b7e5
U-BSD\398121 <398121@SHS-DGDSCS2.beaverton.k12.or.us>
2022-09-19 14:22:08 -0700

    comments added

--- ArmaanPalindrome.cpp
+++ ArmaanPalindrome.cpp
@@ -1,3 +1,9 @@
+/*
+ * Author: Armaan Hajar
+ * Description: Takes a user input and checks if it is a palindrome
+ * Date: 9/19/2022
+ */
+
 #include <iostream>
 #include <cstring>
 
@@ -10,34 +16,33 @@ int main() {
 
   cout << "Enter a Word or Phrase to see if it's a Palindrome! (Max 80 Characters)" << endl;
 
+  // takes user input
   cin.get(str, 80);
   cin.get();
 
+  // removes all non alphanumerical characters from str and copies it into str2
   int count = 0;
   for (int i = 0; i < strlen(str); i++) {
     if (isalnum(str[i]) && str[i] != '\0') {
-      str2[count] = str[i];
+      str2[count] = tolower(str[i]);
       count++;
     }
   }
 
-  // debugging
-  cout << str << endl;
-  cout << str2 << endl;
-
+  // reverses str2 and copies in into str3
   int count2 = 0;
-  for (int i = strlen(str2); i >= 0; i--) {
-    str3[count] = str2[i];
+  for (int i = strlen(str2) - 1; i >= 0; i--) {
+    str3[count2] = str2[i];
     count2++;
-    cout << i << endl;
   }
+  
+  str3[count2] = '\0'; //ensure that string is null terminated
 
-  // debugging
-  cout << str3 << endl;
-
-  if (str2 == str3) {
+  // if str2 and str3 are equal, print out that it is a palindrome
+  if (strcmp(str2, str3) == 0) {
     cout << "Palindrome" << endl;
   }
+  // if not, print out not a palindrome
   else {
     cout << "Not a Palindrome" << endl;
   }
@@ -45,5 +50,4 @@ int main() {
   return 0;
 }
 // hello jeff, you are the best
-
 
